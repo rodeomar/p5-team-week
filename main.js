26b1f01bf46f8492d167bfd84bcef2a71e4be56f
raed <raed.alkhanbashi@gmail.com>
2023-04-26 14:43:06 -0700

    change rectangle size, and take out stroke

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const sketch = function (p) {
   p.setup = function () {
     p.createCanvas(700, 700);
     p.background(0,0,0);
-    p.noStroke(); // Disable stroke for shapes
-    
+    // p.noStroke(); // Disable stroke for shapes 
   }
 
   p.draw = function () {
@@ -24,84 +23,16 @@ const sketch = function (p) {
     p.vertex(x + p.random(-20, 20), y + p.random(-20, 20));
     p.vertex(x + p.random(-20, 20), y + p.random(-20, 20));
     p.vertex(x + p.random(-20, 20), y + p.random(-20, 20));
+        for (let i = 0; i < 100; i++) {
+      p.fill(p.random(255), p.random(255), p.random(255)); // Set random fill color
+      p.rect(x, y, 100, 100); // Draw rectangle
+    }
     p.endShape(p.CLOSE);
-
-    // Check if the shape has been formed to a basketball
-    if (p.frameCount >= 1000000) {
-      p.noLoop(); // Stop the sketch after 100 frames
+    if (p.frameCount >= 100000) {
+      p.noLoop(); // Stop the sketch after 10000 frames
     }
   };
 };
 
 let container = document.getElementById("sketch-container");
 let myP5 = new p5(sketch, container);
-
-
-
-
-// const sketch = function (p) {
-
-//   p.setup = function () {
-//     p.createCanvas(700, 700);
-//     p.background(0,0,0);
-//     p.noStroke(); // Disable stroke for shapes
-    
-//   }
-
-//   p.draw = function () {
- 
-//     let x = p.random(700)
-//     let y = p.random(700)
-//     let colorX = p.map(x, 0, p.width, 0, 255);
-//     let colorY = p.map(y, 0, p.height, 0, 255);
-
-//     // Set random fill color
-//     p.fill(p.random(255), p.random(255), p.random(255)); 
-
-//     // Draw random shape
-//     p.beginShape();
-//     p.vertex(x + p.random(-40, 20), y + p.random(-40, 20));
-//     p.vertex(x + p.random(-40, 20), y + p.random(-40, 20));
-//     p.vertex(x + p.random(-40, 20), y + p.random(-40, 20));
-//     p.vertex(x + p.random(-40, 20), y + p.random(-40, 20));
-//     p.endShape(p.CLOSE);
-
-//   };
-// };
-
-// let container = document.getElementById("sketch-container");
-// let myP5 = new p5(sketch, container);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const sketch = function (p) {
-
-//   p.setup = function () {
-//     p.createCanvas(700, 700);
-//     p.background(0,0,0);
-//   }
-
-//   p.draw = function () {
-//     let x = p.random(800)
-//     let y = p.random(800)
-
-//     for (let i = 0; i < 100; i++) {
-//       p.fill(p.random(255), p.random(255), p.random(255)); // Set random fill color
-//       p.rect(x, y, 200, 200); // Draw rectangle
-//     }
-//   };
-// };
-
-// let container = document.getElementById("sketch-container");
-// let myP5 = new p5(sketch, container);
